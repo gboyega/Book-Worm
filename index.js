1bb5267faff0b3e2ca164324a00445ff9118ed40
gboyega <jaiyeolagboyega@gmail.com>
2019-10-18 22:30:25 +0100

    Created cards and onLoad function to populate window

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-// var cover = `http://covers.openlibrary.org/b/id/${books[i].cover_i}-M.jpg`;
 // var url = `https://openlibrary.org/search.json?q=${...}`;
+var content = document.getElementById("content");
+
 
 window.onload = () => {
     currency = "USD";
-    url = "https://www.googleapis.com/books/v1/volumes?q=search+terms&maxResults=30"
+    url = "https://www.googleapis.com/books/v1/volumes?q=jungle+book&maxResults=40"
         // url = "https://openlibrary.org/search.json?q=the+lord+of+the+rings&page=5";
     var request = new XMLHttpRequest();
     request.open('GET', url);
@@ -11,12 +12,17 @@ window.onload = () => {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
             var books = data.items;
-            console.log(books);
-
-            // content.innerHTML = "";
-            // for (var i = 0; i < coins.length; i++) {
-            //     displayCards(coins, currency, i);
-            // }
+            var eligible = [];
+            for (x = 0; x < books.length; x++) {
+                if (books[x].volumeInfo.imageLinks != null && books[x].volumeInfo.authors != null && books[x].volumeInfo.industryIdentifiers != null) {
+                    eligible.push(books[x]);
+                } else { continue; }
+            }
+            console.log(eligible);
+            content.innerHTML = "";
+            for (var i = 0; i < eligible.length; i++) {
+                displayCards(eligible, i);
+            }
         } else {
             window.alert("There's a problem contacting the server, Please refresh or try again in a few moments.");
         };
@@ -29,25 +35,29 @@ window.onload = () => {
 }
 
 const displayCards = (books, i) => {
+    var info = books[i].volumeInfo;
     var card =
-        `<div class="card col-sm-3 mx-sm-5" style="margin:20px; padding-left:0; padding-right:0;">
-            <img src="${coins[i].iconUrl}" class="card-img-top" alt="${coins[i].name}" style="height:200px; margin:10px;">
-            <h4 class = "card-header">
-                ${coins[i].name}
-            </h4>
-            <div class="card-body">
-                <h5 class="card-title">${coins[i].symbol}</h5>
-                <p class="card-text">${coins[i].description}</p>
-            </div>
-            <ul class="list-group list-group-flush">
-                <li class="list-group-item">Rank: ${coins[i].rank}</li>
-                <li class="list-group-item">Price: ${coins[i].price} ${currency}</li>
-                <li class="list-group-item">24h Price change: ${coins[i].change}%</li>
-                <li class="list-group-item">Market Cap: ${coins[i].marketCap} ${currency}</li>
-                <li class="list-group-item">Circulation: ${coins[i].circulatingSupply}</li>
-            </ul>
-            <div class="card-footer text-muted">
-                ${coins[i].slug}
+        `<div class="card border-secondary mb-3" style="margin:50px;">
+            <div class="row no-gutters">
+                <div class="col-md-2 text-center">
+                    <img src="${info.imageLinks.thumbnail}" class="card-img" alt="cover" style="width:200px; margin:10px;">
+                    <p><a href="${info.previewLink}" class="card-link">Preview</a></p>
+                    <p><a href="${info.infoLink}" class="card-link">Get book</a></p> 
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${info.title}</h5>
+                        <p class="card-text"><small class="text-muted">${info.subtitle}</small></p>
+                        <p class="card-text">${info.description}</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Author: ${info.authors.join(", ")}</li>
+                        <li class="list-group-item">Published: ${info.publishedDate}</li>
+                        <li class="list-group-item">Publisher: ${info.publisher}</li>
+                        <li class="list-group-item">${info.industryIdentifiers[0].type}: ${info.industryIdentifiers[0].identifier}</li>
+                        <li class="list-group-item">Category: ${info.categories}</li>
+                    </ul>
+                </div>
             </div>
         </div>`
     content.insertAdjacentHTML('beforeend', card);
